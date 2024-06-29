626c5d2866d770827c4e8365c34f81a526b0d5de
Juan Collantes <juan@collantes.ec>
2024-06-29 12:42:06 -0500

    checkbox added in form

--- src/app/dashboard/form/page.tsx
+++ src/app/dashboard/form/page.tsx
@@ -16,30 +16,46 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { Calendar } from "@/components/ui/calendar"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
-import { CalendarIcon } from "@radix-ui/react-icons";
-import { Switch } from "@/components/ui/switch"
-const formSchema = z.object({
-  username: z.string().min(2).max(20),
-  email: z.string().email().max(60),
-  gender: z.enum(["male", "female"], {
-    message: "Select gender",
-    required_error: "You need to select a gender.",
-  }),
-  dateOfBirth: z.date({
-    required_error: "A date of birth is required.",
-  }),
-  marketingEmails: z.boolean().default(false),
-  //security_emails: z.boolean().optional(),
-}).refine((data) => data.marketingEmails === true, {
-  message: "You must agree to receive marketing emails.",
-  path: ["marketingEmails"],
-});
-//!Si quiero hacer obligatorio el switch, que va a tener sentido en algunos casos como en la aceptacion de terminos y condiciones,  hago un refine del squema
-
+import { CalendarIcon, ReloadIcon } from "@radix-ui/react-icons";
+import { Switch } from "@/components/ui/switch";
+import { Checkbox } from "@/components/ui/checkbox";
+import Link from "next/link";
+const formSchema = z
+  .object({
+    username: z.string().min(2).max(20),
+    email: z.string().email().max(60),
+    gender: z.enum(["male", "female"], {
+      message: "Select gender",
+      required_error: "You need to select a gender.",
+    }),
+    dateOfBirth: z.date({
+      required_error: "A date of birth is required.",
+    }),
+    marketingEmails: z.boolean().default(false),
+    //security_emails: z.boolean().optional(),
+    mobile: z.boolean().default(false),
+  })
+  //!Si quiero hacer obligatorio el switch, que va a tener sentido en algunos casos como en la aceptacion de terminos y condiciones,  hago un refine del squema
+  .refine((data) => data.marketingEmails === true, {
+    //si no es igual a true o sea no se selecciono mostramos el siguiente mensaje
+    message: "You must agree to receive marketing emails.",
+    //Par aponer el focus en el switch
+    path: ["marketingEmails"],
+  })
+  .refine((data) => data.mobile === true, {
+    //si no es igual a true o sea no se selecciono mostramos el siguiente mensaje
+    message: "You must agree the terms and conditions.",
+    //Par aponer el focus en el switch
+    path: ["mobile"],
+  });
 
 const Page = () => {
   // 1. Define your form.
@@ -48,16 +64,22 @@ const Page = () => {
     defaultValues: {
       username: "",
       email: "",
-      
+      //gender:"female",
+      mobile: false,
     },
   });
-
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const delay = () => {
+    return new Promise((resolve, reject) => setTimeout(resolve, 5000));
+  };
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
+    //console.log(form.formState.isSubmitting)
+    await delay();
     console.log(values);
-  }
+  };
+
   return (
     <div>
       <Form {...form}>
@@ -100,111 +122,146 @@ const Page = () => {
               </FormItem>
             )}
           />
-            {/* Radio group gender */}
-            <FormField
-          control={form.control}
-          name="gender"
-          render={({ field }) => (
-            <FormItem className="space-y-3">
-              <FormLabel>Notify me about...</FormLabel>
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  className="flex flex-col space-y-1"
-                >
-                  <FormItem className="flex items-center space-x-3 space-y-0">
+          {/* Radio group gender */}
+          <FormField
+            control={form.control}
+            name="gender"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Notify me about...</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col space-y-1"
+                  >
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="male" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Male</FormLabel>
+                    </FormItem>
+
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="female" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Female</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Date of Birth */}
+          <FormField
+            control={form.control}
+            name="dateOfBirth"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Date of birth</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
                     <FormControl>
-                      <RadioGroupItem value="male" />
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
                     </FormControl>
-                    <FormLabel className="font-normal">
-                      Male
-                    </FormLabel>
-                  </FormItem>
-
-                  <FormItem className="flex items-center space-x-3 space-y-0">
-                    <FormControl>
-                      <RadioGroupItem value="female" />
-                    </FormControl>
-                    <FormLabel className="font-normal">
-                      Female
-                    </FormLabel>
-                  </FormItem>
-
-                </RadioGroup>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {/* Date of Birth */}
-        <FormField
-          control={form.control}
-          name="dateOfBirth"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Date of birth</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-[240px] pl-3 text-left font-normal",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value ? (
-                        format(field.value, "PPP")
-                      ) : (
-                        <span>Pick a date</span>
-                      )}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("1900-01-01")
-                    }
-                    initialFocus
-                  />
-                </PopoverContent>
-              </Popover>
-              <FormDescription>
-                Your date of birth is used to calculate your age.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        {/* Marketing Email */}
-        <FormField
-              control={form.control}
-              name="marketingEmails"
-              render={({ field }) => (
-                <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm col-span-1 sm:col-span-2">
-                  <div className="space-y-0.5">
-                    <FormLabel>Marketing emails</FormLabel>
-                    <FormDescription>
-                      Receive emails about new products, features, and more.
-                    </FormDescription>
-                  </div>
-                  <FormControl>
-                    <Switch
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
                     />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          <Button type="submit">Submit</Button>
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>
+                  Your date of birth is used to calculate your age.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Marketing Email */}
+          <FormField
+            control={form.control}
+            name="marketingEmails"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm col-span-1 sm:col-span-2">
+                <div className="space-y-0.5">
+                  <FormLabel>Marketing emails</FormLabel>
+                  <FormDescription>
+                    Receive emails about new products, features, and more.
+                  </FormDescription>
+                </div>
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="mobile"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow col-span-1 sm:col-span-2">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <div className="space-y-1 leading-none">
+                  <FormLabel>
+                    Use different settings for my mobile devices
+                  </FormLabel>
+                  <FormDescription>
+                    You can manage your mobile notifications in the{" "}
+                    <Link href="/examples/forms">mobile settings</Link> page.
+                  </FormDescription>
+                </div>
+              </FormItem>
+            )}
+          />
+
+          <div
+            className={cn(
+              "flex justify-center items-center col-span-1 sm:col-span-2",
+              form.formState.isSubmitting && "border border-red-200"
+            )}
+          >
+            {!form.formState.isSubmitting ? (
+              <Button variant="success" type="submit">
+                Submit
+              </Button>
+            ) : (
+              <Button disabled>
+                <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>
+            )}
+          </div>
         </form>
       </Form>
     </div>
